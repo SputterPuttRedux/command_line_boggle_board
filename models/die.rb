382024293cc69d133b134710a3441925c08f4c6c
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-06-16 21:56:49 -0400

    Create die model and letters attrs

---
+++ models/die.rb
@@ -0,0 +1,23 @@
+class Die
+  attr_accessor :die
+  def initialize
+    @letters = [
+      %w(AAEEGN),
+      %w(ELRTTY),
+      %w(AOOTTW),
+      %w(ABBJOO),
+      %w(EHRTVW),
+      %w(CIMOTV),
+      %w(DISTTY),
+      %w(EIOSST),
+      %w(DELRVY),
+      %w(ACHOPS),
+      %w(HIMNQU),
+      %w(EEINSU),
+      %w(EEGHNW),
+      %w(AFFKPS),
+      %w(HLNNRZ),
+      %w(DEILRX)
+    ]
+  end
+end
