86c4901a34c92dc7779b37ab2e636b0f60120acb
SputterPuttRedux <dstanfield.esq@gmail.com>
2015-06-17 22:41:18 -0400

    Require model and view files. Add accessors. Create better board population method. Begin grid solver module

--- controllers/game.rb
+++ controllers/game.rb
@@ -1,18 +1,45 @@
+require_relative '../controllers/game.rb'
+require_relative '../models/die.rb'
+require_relative '../models/board.rb'
+
 class Game
+  attr_accessor :board, :die
   def initialize
     @board = Board.new
     @die = Die.new
   end
 
   def shake!
-    16.times { @board.slots << place_a_die }
-    @board
+    slots_count.times do
+      @board.slots << place_a_die
+    end
+    @board.slots
   end
 
   private
 
   def place_a_die
-    @die.letters.shuffle.pop
+    @die.letters.shuffle.pop.sample
   end
 
+  def slots_count
+    @die.letters.count
+  end
+end
+
+module GridSolver
+  class Boggle
+    #only allows for even grid
+    def initialize(grid_size)
+      @grid_size = grid_size
+    end
+
+    def corner_coordinates
+
+    end
+
+    def at_corner?
+    end
+
+  end
 end
